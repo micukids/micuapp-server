8916b403ca6375c58653e795b6c0dd4191ee219d
Elvia Benedith <ejbenedith@gmail.com>
2023-03-18 23:16:09 +0100

    roles and permissions

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -60,7 +60,14 @@ class AuthController extends Controller
                     'message'=>'Email o password no válido.'
                 ]);
             } else {
-                $token = $user->createToken($user->email.'_Token')->plainTextToken;
+
+                if( $user->role_as == 1){
+                    $token =  $user->createToken($user->email.'_AdminToken', ['server:admin'])->plainTextToken;
+                }else {
+
+                    $token = $user->createToken($user->email.'_Token', [''])->plainTextToken;
+                }
+                
 
                 return response()->json([
                     'status'=>200,
--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,16 @@ use App\Http\Controllers\API\AuthController;
 Route::post('register', [AuthController::class,'register']);
 Route::post('login', [AuthController::class,'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum', 'IsAPIAdmin'])->group(function () {
+
+    Route::get('/checkingAuthenticated', function () {
+        return response()->json(['message'=>'estas dentro', 'status'=>200], 200);
+    });
+
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
     Route::post('logout', [AuthController::class,'logout']);
 });
 
