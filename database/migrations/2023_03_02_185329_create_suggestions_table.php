5b00469df3f0505b6ea0f6d288053a4d13f9b9ba
Elvia Benedith <ejbenedith@gmail.com>
2023-03-21 15:17:19 +0100

    Suggestions seeder

--- database/migrations/2023_03_02_185329_create_suggestions_table.php
+++ database/migrations/2023_03_02_185329_create_suggestions_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('suggestions', function (Blueprint $table) {
             $table->id();
-            $table->string('type');
+            $table->string('title');
             $table->string('image');
             $table->string('url');
             $table->string('description');
---
+++ database/seeders/SuggestionSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SuggestionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('suggestions')->insert([
+           'title'=>"Flash cards",
+           'image'=>"https://images.squarespace-cdn.com/content/v1/6029f224610d6f1042e8e48e/1613995847616-4VTJIKLXM6QLHV7KGH9S/FotoABC_01.JPG",
+           'url'=>"https://www.micukids.com/",
+           'description'=>"Tartejas borrables bilingües del abecedario",
+        ]);
+
+        DB::table('suggestions')->insert([
+            'title'=>"Bluey",
+            'image'=>"https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/814quYUniGL.jpg",
+            'url'=>"https://youtu.be/7dUpS_8ciRc",
+            'description'=>"Serie animada australiana para preescolares",
+         ]);
+
+         DB::table('suggestions')->insert([
+            'title'=>"A, B, C, CH, D",
+            'image'=>"https://pbs.twimg.com/media/Ey2-9CbWQAAf3Ct?format=jpg",
+            'url'=>"https://www.youtube.com/watch?v=CjKX3X-ZKzw",
+            'description'=>"Marta Gómez, canción del abecedario para niños en castellano e Inglés para MicuKids",
+         ]);
+    }
+}
