8d8b379b6663dfc7ceda1a1410d813de169fcaf9
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-03-21 09:50:12 +0100

    name max length 9 char

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -13,7 +13,7 @@ class AuthController extends Controller
     public function register(Request $request){
        
         $validator = Validator::make($request->all(),[
-            'name'=>'required|min:2|max:12',
+            'name'=>'required|min:2|max:9',
             'parent'=>'required|min:2|max:12',
             'email'=> 'required|email|max:191|unique:users,email',
             'password'=>'required|min:8'
