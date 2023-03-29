cd6dd327ea80be476889a55240e03840fd1972c1
KaroVillarraga <karovilla@gmail.com>
2023-03-29 22:33:59 +0200

    DownloadSeeder updated

--- database/migrations/2023_03_02_185901_create_downloads_table.php
+++ database/migrations/2023_03_02_185901_create_downloads_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
     {
         Schema::create('downloads', function (Blueprint $table) {
             $table->id();
-            $table->string('type');
+            $table->string('thumb');
             $table->string('url');
+            $table->string('description');
             $table->timestamps();
         });
     }
--- database/seeders/DownloadSeeder.php
+++ database/seeders/DownloadSeeder.php
@@ -13,6 +13,40 @@ class DownloadSeeder extends Seeder
      */
     public function run(): void
     {
+        DB::table('downloads')->insert([
+            'thumb'=>"https://play.micukids.com/play/dl/thumb_01_poster.jpg",
+            'url'=>"https://play.micukids.com/play/dl/01_Poster_Micukids.pdf",
+            'description'=>"Poster del abecedario",
+         ]);
 
+         DB::table('downloads')->insert([
+            'thumb'=>"https://play.micukids.com/play/dl/thumb_A.jpg",
+            'url'=>"https://play.micukids.com/play/dl/A_micukids.pdf",
+            'description'=>"Colorea la letra A",
+         ]);
+
+         DB::table('downloads')->insert([
+            'thumb'=>"https://play.micukids.com/play/dl/thumb_E.jpg",
+            'url'=>"https://play.micukids.com/play/dl/E_micukids.pdf",
+            'description'=>"Colorea la letra E",
+         ]);
+
+         DB::table('downloads')->insert([
+            'thumb'=>"https://play.micukids.com/play/dl/thumb_I.jpg",
+            'url'=>"https://play.micukids.com/play/dl/I_micukids.pdf",
+            'description'=>"Colorea la letra I",
+         ]);
+
+         DB::table('downloads')->insert([
+            'thumb'=>"https://play.micukids.com/play/dl/thumb_O.jpg",
+            'url'=>"https://play.micukids.com/play/dl/O_micukids.pdf",
+            'description'=>"Colorea la letra O",
+         ]);
+
+         DB::table('downloads')->insert([
+            'thumb'=>"https://play.micukids.com/play/dl/thumb_U.jpg",
+            'url'=>"https://play.micukids.com/play/dl/U_micukids.pdf",
+            'description'=>"Colorea la letra U",
+         ]);
     }
 }
